ca5c254929470d98444d377d9338984284b66fd0
ivn-svn <isvetllin@gmail.com>
2023-06-07 22:45:36 +0300

    add tasks to the admin panel

--- django101/tasks/models.py
+++ django101/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    # varchar
+    task_title = models.CharField(max_length=50, null=False)
+    task_text = models.TextField()
+    # int
+    priority = models.IntegerField(default=1)
