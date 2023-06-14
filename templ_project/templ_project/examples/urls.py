bb79d64dddb149fe4cd497b98618c7a64883ecdd
ivn-svn <isvetllin@gmail.com>
2023-06-14 13:39:30 +0300

    add error branch

--- templ_project/templ_project/examples/urls.py
+++ templ_project/templ_project/examples/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("templ_project.examples.urls")),
+    path('', views.index, name='index'),
+    path('contact/', views.contact_view, name="contact page"),
 ]
--- templ_project/templ_project/examples/views.py
+++ templ_project/templ_project/examples/views.py
@@ -1,11 +1,12 @@
 import datetime
 import random
+from django.urls import reverse
 
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from . import views
 import random
 # Create your views here.
-
 class Student():
     def __init__(self, name, age):
         self.name = name
@@ -14,8 +15,8 @@ class Student():
     def get_age(self):
         return self.age
 
-some_student = Student("John", 20)
 
+some_student = Student("John", 20)
 
 
 def index(request):
@@ -23,9 +24,25 @@ def index(request):
         "title": "Home",
         "random_int": random.random(),
         "nested": {
-            "second": "second value"
+            "second": "second value",
         },
-        "student": some_student.get_age(),
+        "student_age": some_student.get_age(),
+        "students": ["Kalin", "Ivan", "Pesho", "Pesho", "Mariya"],
+        "students_second": [],
         "now": datetime.datetime.now(),
+        "numbers": [1, 2, 3, 4, 5, 6, 7],
+        "name": "Now hardcoded",
+        "students_obj": [
+            Student('Lili', 23),
+            Student('Moni', 20),
+            Student('Yavor', 30)
+        ],
+        "logged_in": True
     }
+
+    # context + template = HtmlResponse('<html>...</html>')
     return render(request, 'examples/index.html', context=context)
+
+
+def contact_view(request):
+    return redirect('contact page')
