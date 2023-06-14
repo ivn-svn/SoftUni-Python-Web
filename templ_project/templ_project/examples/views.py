dfee4d246f0f0eda0d1c2b56078235e5f056a75e
ivn-svn <isvetllin@gmail.com>
2023-06-14 11:51:57 +0300

    add progress Python Web June 2023

--- templ_project/templ_project/examples/views.py
+++ templ_project/templ_project/examples/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+import datetime
+import random
 
+from django.shortcuts import render
+from . import views
+import random
 # Create your views here.
+
+class Student():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_age(self):
+        return self.age
+
+some_student = Student("John", 20)
+
+
 
 def index(request):
     context = {
-        "title": "Home"
+        "title": "Home",
+        "random_int": random.random(),
+        "nested": {
+            "second": "second value"
+        },
+        "student": some_student.get_age(),
+        "now": datetime.datetime.now(),
     }
     return render(request, 'examples/index.html', context=context)
